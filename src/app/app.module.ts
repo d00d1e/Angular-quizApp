4d9c41780676a1621523cb913a169a030ed66289
d00d1e <lee.julianay@gmail.com>
2020-06-04 13:13:49 -0700

    Created form component
    question-form.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionFormComponent
   ],
   imports: [
     BrowserModule,
